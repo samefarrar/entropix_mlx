39e16984125770f2191ea2ff2107c0f7864e6dbe
samefarrar <67292186+samefarrar@users.noreply.github.com>
2024-10-05 15:43:16 +0100

    chore: Cleanup download imports

--- mlx_download_weights.py
+++ mlx_download_weights.py
@@ -1,15 +1,16 @@
 from pathlib import Path
 from huggingface_hub import snapshot_download
 
-# Specify the checkpoint
-checkpoint = "meta-llama/Llama-3.2-1B-Instruct"
+if __name__ == "__main__":
+    # Specify the checkpoint
+    checkpoint = "meta-llama/Llama-3.2-1B-Instruct"
 
-if not Path("weights/1B-Instruct").exists():
-    print(f"Can't find the weights for {checkpoint}. Downloading...")
-    model_path = snapshot_download(
-        repo_id=checkpoint, local_dir=Path("weights/1B-Instruct")
-    )
-else:
-    print(f"Weights for {checkpoint} already exist at weights/1B-Instruct.")
+    if not Path("weights/1B-Instruct").exists():
+        print(f"Can't find the weights for {checkpoint}. Downloading...")
+        model_path = snapshot_download(
+            repo_id=checkpoint, local_dir=Path("weights/1B-Instruct")
+        )
+    else:
+        print(f"Weights for {checkpoint} already exist at weights/1B-Instruct.")
 
-print(f"Weights for {checkpoint} are downloaded.")
+    print(f"Weights for {checkpoint} are downloaded.")
