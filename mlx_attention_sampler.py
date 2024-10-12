05b9bd9930e29855defc43a63e2a723b609adcc0
samefarrar <67292186+samefarrar@users.noreply.github.com>
2024-10-12 14:55:04 +0100

    feat: Adjust high ent and var-ent thresholds to better fit data

--- mlx_attention_sampler.py
+++ mlx_attention_sampler.py
@@ -14,18 +14,19 @@ class SamplerConfig(BaseModel):
     low_ent_thresh: float = 0.1
     low_vent_thresh: float = 0.1
     med_ent_thresh: float = 3.0
-    high_ent_thresh: float = 5.0
-    high_vent_thresh: float = 5.0
+    med_vent_thresh: float = 3.0
+    high_ent_thresh: float = 4.0
+    high_vent_thresh: float = 7.0
 
     # TODO this is a bit of a nasty mess, but also makes all the hyperparameters visible
     helv_attn_ent_offset: float = 1.3
     helv_attn_ent_coef: float = 0.2
 
     lehv_interaction_strength_offset: float = 1.2
-    lehv_interaction_strength_coef: float = 0.3
+    lehv_interaction_strength_coef: float = 0.1
 
-    hehv_attn_ent_coef: float = 0.2
-    hehv_attn_vent_offset: float = 2.0
+    hehv_attn_ent_coef: float = 0.2 / 0.29
+    hehv_attn_vent_offset: float = 1.3
     hehv_attn_vent_coef: float = 0.5
 
     # TODO not convinced this should
@@ -41,8 +42,8 @@ class SamplerConfig(BaseModel):
     ada_top_k_agree: float = 0.2
     ada_min_p: float = 0.5
     ada_score_logits_ent: float = 0.1
-    ada_score_attn_ent: float = 0.2
+    ada_score_attn_ent: float = 0.2 / 0.29
     ada_score_logits_vent: float = 0.3
-    ada_score_attn_vent: float = 0.4
+    ada_score_attn_vent: float = 0.4 * 100
     ada_score_agree: float = 0.5
     ada_score_int: float = 0.6
